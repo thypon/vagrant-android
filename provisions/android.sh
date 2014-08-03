de847a2cfe87a51f0da9ed61b43efdc1470f5da1
Andrea Brancaleoni <miwaxe+git@gmail.com>
2014-08-03 13:19:07 +0200

    Added some mod dependencies (AOKP, Mokee, Cyanogenmod, ...)

--- provisions/android.sh
+++ provisions/android.sh
@@ -13,7 +13,8 @@ sudo -E apt-get install -y -q git gnupg flex bison gperf build-essential \
   zip curl libc6-dev libncurses5-dev:i386 x11proto-core-dev \
   libx11-dev:i386 libreadline6-dev:i386 libgl1-mesa-glx:i386 \
   libgl1-mesa-dev g++-multilib mingw32 tofrodos \
-  python-markdown libxml2-utils xsltproc zlib1g-dev:i386
+  python-markdown libxml2-utils xsltproc zlib1g-dev:i386 \
+  bc schedtool
 ## Links 32bit libGL to the expected path
 sudo ln -s /usr/lib/i386-linux-gnu/mesa/libGL.so.1 /usr/lib/i386-linux-gnu/libGL.so
 ## Installs && Set-ups CCache
