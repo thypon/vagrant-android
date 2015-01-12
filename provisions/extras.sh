f99a131f4b22401f8a4217975ca0ab5bbd8d633a
Andrea Brancaleoni <miwaxe+git@gmail.com>
2015-01-12 21:35:07 +0100

    Added libxi6 & SWT to install some X11 apps

--- provisions/extras.sh
+++ provisions/extras.sh
@@ -1,4 +1,5 @@
 export DEBIAN_FRONTEND=noninteractive
 # Prepare Extras
 ## Installs Utilities
-sudo -E apt-get install -q -y htop dtach tig
+sudo -E apt-get install -q -y htop dtach tig libxi6 libswt-gtk-3-java
+ln -s /usr/lib/jni/libswt-* /home/vagrant/.swt/lib/linux/x86_64/
